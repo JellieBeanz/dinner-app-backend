84d2145ef75dd9f08974a0eeef70b4d5e745a4b8
JellieBeanz <shaneoneill1995@gmail.com>
2025-01-14 23:33:58 +0100

    DATABASE_URL

--- config/database.ts
+++ config/database.ts
@@ -6,11 +6,7 @@ export default ({ env }: { env: {
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_dev'),
-      user: env('DATABASE_USERNAME', 'strapi_user'),
-      password: env('DATABASE_PASSWORD', 'password'),
+      connectionString: env('DATABASE_URL'), 
       ssl: env.bool('DATABASE_SSL', false), // Ensure this is set to false
     },
   },
